9661ac0c33a1cfd2e843c577f2d1a3896e74afdd
Lu♡ <luizagomes10082009@gmail.com>
2026-03-02 08:45:48 -0300

    Add files via upload

---
+++ Aula02/exerciciosaula02.js
@@ -0,0 +1,73 @@
+//Questão A da atividade 1
+var a = "10";
+var b = "20";
+
+console.log(b)
+b = 5; 
+
+console.log(a, b); 
+
+//Questão B da atividade 1
+
+a = 30;
+b = 20;
+
+c = a + b;
+
+console.log(c)
+
+b = 10;
+
+console.log(b, c)
+console.log(a, b, c)
+
+//Questão C da atividade 1
+
+var a = "10";
+var b = "20";
+
+var c = a;
+var b = c;
+
+var a = b;
+
+console.log(a, b, c)
+
+
+//Questão D da atividade 1
+
+a = 10;
+b = a + 1;
+a = b + 1;
+
+b = a + 1;
+
+console.log(a)
+a = b + 1;
+console.log(a, b)
+
+
+// Exercicio 02
+
+var nota1 = 10 ;
+var nota2 = 7 ;
+var nota3 = 5 ;
+
+var media = nota1*2 + nota2*3 + nota3*5 
+
+mediafinal = media / 10
+
+console.log(mediafinal)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Aula02/exerciciosaula03.js
@@ -0,0 +1,13 @@
+
+let resposta = confirm("Você deseja continua?");
+if (resposta) {
+    alert ("Você escolheu continuar");
+}else {
+    alert ("Você cancelou a ação");
+}
+
+
+//Prompt
+
+let nome = prompt ("Qual é seu nome?");
+alert ("Olá, " + nome + "!");
